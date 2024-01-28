26c89aae8bca4d044b80611c1d8356045b09bf9b
zen-pb <111935236+zen-pb@users.noreply.github.com>
2024-01-28 20:46:17 +0800

    Change title bar SSG to SSC

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require_once './includes/login.view.php';
    <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>ISU Cauayan Campus | SSG Election Portal</title>
+      <title>ISU Cauayan Campus | SSC Election Portal</title>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link
