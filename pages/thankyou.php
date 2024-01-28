53f6e97a082a04834ea66511f01f5539f88d7dbf
zen-pb <111935236+zen-pb@users.noreply.github.com>
2024-01-28 20:42:30 +0800

    Add thankyou.php to pages/ folder

---
+++ pages/thankyou.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thank You</title>
+</head>
+
+<body>
+    <h1>Thank You for Voting!</h1>
+
+    <h2>Vote Summary:</h2>
+    <p>President:
+        <?php echo $_GET['president']; ?>
+    </p>
+    <p>Vice President:
+        <?php echo $_GET['vicePresident']; ?>
+    </p>
+    <p>Secretary:
+        <?php echo $_GET['secretary']; ?>
+    </p>
+    <p>Treasurer:
+        <?php echo $_GET['treasurer']; ?>
+    </p>
+    <p>Auditor:
+        <?php echo $_GET['auditor']; ?>
+    </p>
+    <p>Accountant:
+        <?php echo $_GET['accountant']; ?>
+    </p>
+</body>
+
+</html>
