c285c2f0ac327fa58d360ca6c48fdc2e3cd0f624
zen-pb <111935236+zen-pb@users.noreply.github.com>
2024-02-02 21:55:51 +0800

    Add logo to title bar

--- pages/thankyou.php
+++ pages/thankyou.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../assets/img/ISU-logo.png" type="image/icon type">
     <title>Thank You</title>
 </head>
 
